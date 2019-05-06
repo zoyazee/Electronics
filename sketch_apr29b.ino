fba32d2d37f2bd398aace631d8d361337dac193f
zoyazee <47854404+zoyazee@users.noreply.github.com>
2019-05-06 20:02:11 +0300

    Add files via upload
    Adding a file

---
+++ sketch_apr29b.ino
@@ -0,0 +1,29 @@
+int TRIG=9;
+int ECHO=5;
+float Time;
+float Distance;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(TRIG,OUTPUT);
+  pinMode(ECHO,INPUT);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  digitalWrite(TRIG,LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG,LOW);
+
+  Time = pulseIn(ECHO,HIGH);
+  Distance=(Time*0.0343)/2;
+
+  Serial.print(Distance);
+  Serial.println("cm");
+  delay(1000);
+  // put your main code here, to run repeatedly:
+
+}
