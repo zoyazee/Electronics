42057428eaf6e04dc37b2d8015968cc4f2889955
zoyazee <47854404+zoyazee@users.noreply.github.com>
2019-05-06 19:56:52 +0300

    Add files via upload
    Adding a file

---
+++ sketch_apr29a.ino
@@ -0,0 +1,23 @@
+int LEDGREEN=9;
+int PIR=5;
+int Readvalue;
+void setup() {
+  pinMode(PIR,INPUT);
+  pinMode(LEDGREEN,OUTPUT);
+  
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  Readvalue=digitalRead(PIR);
+  if(Readvalue==HIGH)
+  {digitalWrite(LEDGREEN,HIGH);
+  }
+  else{
+    digitalWrite(LEDGREEN,LOW);
+    }
+  
+  // put your main code here, to run repeatedly:
+
+}
